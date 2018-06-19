e291b8a19a9195b99807844f4e0839d64a2a0a75
ttlg <dev.ttlg@gmail.com>
2018-06-19 18:21:46 +0900

    refactor

--- in-place-config/link_config.sh
+++ in-place-config/link_config.sh
@@ -1,6 +1,7 @@
 
-ln -sf ~/dotfiles/in-place-config/vim.conf            ~/.vimrc
-ln -sf ~/dotfiles/in-place-config/tmux.conf           ~/.tmux.conf
-ln -sf ~/dotfiles/in-place-config/vscode.setting      ~/Library/Application\ Support/Code/User/settings.json 
-ln -sf ~/dotfiles/in-place-config/vscode.keybindings  ~/Library/Application\ Support/Code/User/keybindings.json
+
+ln -sf ${DOTPATH}/in-place-config/vim.conf            ~/.vimrc
+ln -sf ${DOTPATH}/in-place-config/tmux.conf           ~/.tmux.conf
+ln -sf ${DOTPATH}/in-place-config/vscode.setting      ~/Library/Application\ Support/Code/User/settings.json 
+ln -sf ${DOTPATH}/in-place-config/vscode.keybindings  ~/Library/Application\ Support/Code/User/keybindings.json
 

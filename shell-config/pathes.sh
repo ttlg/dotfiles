c408df6f0909ef79769bb8da854305b2b6ce602a
ttlg <dev.ttlg@gmail.com>
2018-06-19 17:48:42 +0900

    add nodeberw to path

--- shell-config/pathes.sh
+++ shell-config/pathes.sh
@@ -9,6 +9,8 @@ export PATH="$HOME/.local/bin:$PATH"
 export PATH=${HOME}/.cargo/bin:${PATH}
 export PATH=/usr/local/bin/:${PATH}
 
+export PATH="$PATH:$HOME/.nodebrew/current/bin"
+
 export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 
 # pyenv

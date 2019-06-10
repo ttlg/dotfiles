902be2454fd992ff920d0fd9a928661a485031ac
ttlg <dev.ttlg@gmail.com>
2019-06-10 16:05:32 +0900

    flutter dir

--- shell-config/pathes.sh
+++ shell-config/pathes.sh
@@ -11,6 +11,7 @@ export PATH="$HOME/Library/Android/sdk/tools/:$PATH"
 
 # Flutter
 export PATH="$HOME/Library/flutter/bin:$PATH"
+export FLUTTER_FRAMEWORK_DIR="$HOME/Library/flutter/bin/cache/artifacts/engine/ios"
 
 # Rust
 export PATH=${HOME}/.cargo/bin:${PATH}
@@ -21,10 +22,14 @@ export PATH="$PATH:$HOME/.nodebrew/current/bin"
 # VSCode
 export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 
+# Flutter
+export PATH="$PATH":"$HOME/Library/flutter/.pub-cache/bin"
+
 # pyenv
 if [ -e $HOME/.pyenv ];then
 	export PYENV_ROOT="$HOME/.pyenv"
 	export PATH="$PYENV_ROOT/bin:$PATH"
 	eval "$(pyenv init -)"
 fi
+
 

c717e6ae8e30f17b49c38db0ad6ba2e9a7a65681
ttlg <dev.ttlg@gmail.com>
2020-10-18 20:00:50 +0900

    :fire: remove unused

--- load-antigen.sh
+++ load-antigen.sh
@@ -4,7 +4,6 @@ antigen bundle git
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle zsh-users/zsh-completions
-antigen bundle zsh-users/zsh-completions
 
 antigen apply
 
